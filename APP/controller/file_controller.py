343a2fbae01a8782747c082fe8537f3a1901272b
NicolasSanchez99 <68711971+NicolasSanchez99@users.noreply.github.com>
2022-03-31 13:49:37 -0500

    V1

---
+++ APP/controller/file_controller.py
@@ -0,0 +1,11 @@
+file = "log_trabajoya_prod.log"
+lines = []
+with open(file) as aux:
+    for i, line in enumerate(aux.readlines()):
+        if i ==10:
+            break
+        lines.append(line)
+len(lines)
+lineO = lines[1]
+for i, lista in enumerate(lines):
+    print(lines[i])
---
+++ APP/model/file.py
@@ -0,0 +1,18 @@
+class file:
+    fecha= ''
+    tipo_mensaje= ''
+    archivo = ''
+    linea = ''
+
+    def print_fecha(self):
+        print(self.name)
+
+    def print_tipo_mensaje(self):
+        print(self.tipo_mensaje)
+
+    def print_archivo(self):
+        print(self.archivo)
+
+    def print_linea(self):
+        print(self.linea)
+
